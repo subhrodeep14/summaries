b450b8ac1fc73a1e01c95d430312a853996b3c96
Subhrodeep Acharya <subhrodeepacharya14@gmail.com>
2025-04-08 23:43:12 +0530

    route.ts added

---
+++ app/api/uploadthing/route.ts
@@ -0,0 +1,7 @@
+import { createRouteHandler } from "uploadthing/next";
+import { ourFileRouter, OurFileRouter } from "./core";
+
+
+export const { GET, POST } = createRouteHandler({router:ourFileRouter ,
+    //config
+});
